669fb1c1359c3744c2cbdd3be2c32dc98ba9480b
Gary <gary.mathews.93@gmail.com>
2012-12-26 09:15:46 -0800

    Add storage selection (1/2)
    Patch Set 2 : Cleaned a few very minor things up.
    
    Patch Set 3 : Fixed indentation.
    
    Patch Set 4 : Changed how 'sStorage' is accessed, it is now a private static member.
    
    Patch Set 5/6 : Fixed indentation. (yet again...)
    
    Change-Id: I3a86a4698d07cede8f93f5fbd8f92b9464d82b90
    
    Conflicts:
    	res/xml/video_preferences.xml

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -430,6 +430,8 @@ public class VideoModule implements CameraModule,
         mPrefVideoEffectDefault = mActivity.getString(R.string.pref_video_effect_default);
         resetEffect();
 
+        Storage.setStorage(CameraSettings.readStorage(mPreferences));
+
         // Power shutter
         mActivity.initPowerShutter(mPreferences);
 
@@ -1443,7 +1445,7 @@ public class VideoModule implements CameraModule,
         // Used when emailing.
         String filename = title + convertOutputFormatToFileExt(outputFileFormat);
         String mime = convertOutputFormatToMimeType(outputFileFormat);
-        String path = Storage.DIRECTORY + '/' + filename;
+        String path = Storage.generateDirectory() + '/' + filename;
         String tmpPath = path + ".tmp";
         mCurrentVideoValues = new ContentValues(7);
         mCurrentVideoValues.put(Video.Media.TITLE, title);
@@ -2340,6 +2342,13 @@ public class VideoModule implements CameraModule,
 
             // Check if the current effects selection has changed
             if (updateEffectSelection()) return;
+
+            String storage = CameraSettings.readStorage(mPreferences);
+            if (!storage.equals(Storage.getStorage())) {
+                Storage.setStorage(storage);
+                mActivity.updateStorageSpaceAndHint();
+                mActivity.reuseCameraScreenNail(!mIsVideoCaptureIntent);
+            }
 
             readVideoPreferences();
             showTimeLapseUI(mCaptureTimeLapse);
